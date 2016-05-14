21431465c8760b4ae35a48284b22251fa58a8321
Cieniu <marcin@laret.pl>
2016-05-14 22:20:40 +0200

    Add speech class

---
+++ src/speech.py
@@ -0,0 +1,21 @@
+import urllib2
+
+class Speech():
+    def __init__(self):
+        self.data = []
+        
+    def hello(self, text):
+        self.create_speech(text)
+
+    def filter_spaces(self, text):
+        return text.replace(" ", "%20")
+
+    @staticmethod
+    def create_speech(self, text):
+        url_speak = "http://127.0.0.1:8000/speech?text=" + self.filter_spaces(text)
+        response = urllib2.urlopen(url_speak)
+        print response
+    
+
+if __name__ == "__main__":
+    Speech.hello('Hello World!')

c8fd6d0494c4d571b6cd16d9ae498288351dc679
Cieniu <marcin@laret.pl>
2016-05-14 21:24:40 +0200

    add keys

--- src/pymove.py
+++ src/pymove.py
@@ -15,14 +15,22 @@ class PyMove:
         pygame.init()
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-                    print "dupa down"
-                elif event.type == pygame.KEYUP and event.key == pygame.K_d:
-                    print "dupa up"
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_F1:
-                    print "dupa down"
-                elif event.type == pygame.KEYUP and event.key == pygame.K_F1:
-                    print "dupa up"
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                    print "Up down"
+                elif event.type == pygame.KEYUP and event.key == pygame.K_UP:
+                    print "Up up"
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                    print "Down down"
+                elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+                    print "Down up"
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                    print "Left down"
+                elif event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
+                    print "Left up"
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                    print "Right down"
+                elif event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
+                    print "Right up"
 
 if __name__ == '__main__':
     PyMove().start()

b6ddba6d52a933eb8a2ede96804a5f443c1b6275
Cieniu <marcin@laret.pl>
2016-05-14 22:38:47 +0200

    fix speech

--- src/speech.py
+++ src/speech.py
@@ -3,17 +3,17 @@ import urllib2
 class Speech(object):
     def __init__(self, text):
         self.text = text
-        self.create_speech(self.text)
+        self.create_speech()
         
     def hello(self, text):
-        self.create_speech(text)
+        self.create_speech()
 
-    def filter_spaces(self, text):
-        return text.replace(" ", "%20")
+    def filter_spaces(self):
+        return self.text.replace(" ", "%20")
 
     @staticmethod
     def create_speech(self):
-        url_speak = "http://127.0.0.1:8000/speech?text=" + self.filter_spaces(self.text)
+        url_speak = "http://127.0.0.1:8000/speech?text=" + self.filter_spaces()
         response = urllib2.urlopen(url_speak)
         print response
     

b9bb49b791652892b19ae868eeafe92162411828
Cieniu <marcin@laret.pl>
2016-05-14 21:17:01 +0200

    test

--- src/pymove.py
+++ src/pymove.py
@@ -9,19 +9,16 @@ class PyMove:
 
     def __init__(self):
         self.data = []
+        self.move = false
 
     def start(self):
         pygame.init()
-        while 1:
+        while True:
             for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == K_w:
-                        #keys[0]=True
-                        print "K up"
-                if event.type == pygame.KEYUP:
-                    if event.key == K_w:
-                        keys[0]=True
-                        print "K up"
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                    print "dupa down"
+                elif event.type == pygame.KEYUP and event.key == pygame.K_d:
+                    print "dupa up"
 
 if __name__ == '__main__':
     PyMove().start()

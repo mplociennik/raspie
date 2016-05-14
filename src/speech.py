4f1bc9b722fb631872e3d0d15482cf9ed955207b
Cieniu <marcin@laret.pl>
2016-05-14 22:36:24 +0200

    fix speech

--- src/speech.py
+++ src/speech.py
@@ -12,7 +12,7 @@ class Speech(object):
         return text.replace(" ", "%20")
 
     @staticmethod
-    def create_speech(self, text):
+    def create_speech(self):
         url_speak = "http://127.0.0.1:8000/speech?text=" + self.filter_spaces(self.text)
         response = urllib2.urlopen(url_speak)
         print response

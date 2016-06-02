2731e822ce84f4be924d4829bcf50360c1c7a2f5
Cieniu <marcin@laret.pl>
2016-06-02 17:03:47 +0200

    test

--- src/pymove.py
+++ src/pymove.py
@@ -6,7 +6,7 @@ from pygame.locals import *
 from speech import Speech
 import time
 
-gpio.cleanup()
+gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 gpio.setup(7, gpio.OUT)
 gpio.setup(11, gpio.OUT)

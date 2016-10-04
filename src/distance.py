2cc8998da61c48ad64d2e4cdcd9776ea15fca4b1
Cieniu <mplociennik@users.noreply.github.com>
2016-10-04 11:32:34 +0200

    Update distance class

--- src/distance.py
+++ src/distance.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python2
 #encoding: utf-8
+import RPi.GPIO as GPIO
+import time
 
 
 class Distance:
-   print "test" 
+    """
+    For detecting distanse.
+    """
+
+    GPIO.setmode(GPIO.BOARD)
+    TRIG = 7
+    ECHO = 12
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.output(ECHO.GPIO.IN)
+    
+    def __init__(self):
+        pass
+
+    def detect(self):
+        time.sleep(0.1)
+        print "Starting detection..."
+        GPIO.output(TRIG,1)
+        time.sleep(0.00001)
+        GPIO.output(TRIG,0)
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO) == 1:
+            pulse_stop = time.time()
+        distance = (pulse_stop - pulse_start) * 17150
+        distance = round(distance, 2)
+        GPIO.cleanup()
+        return distance
 
 if __name__ == "__main__":
-    print "Hello World"
+    distance = Distance()
+    distance.detect()
+
+

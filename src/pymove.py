fee9ef145b3e62cf9b84c619c34275bcc0c97336
Cieniu <marcin@laret.pl>
2016-10-12 19:17:33 +0200

    test

--- src/pymove.py
+++ src/pymove.py
@@ -41,7 +41,7 @@ class PyMove:
     """
     For controlling motors by gpio raspberry and keyboard.
     """
-    autopilot = multiprocessing.Value('autopilot', False)
+    autopilot = False
     def __init__(self):
         self.data = []
         self.move = False

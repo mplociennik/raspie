cc6155264dd996d24deb81653c2ca635a8b6cd4e
Cieniu <marcin@laret.pl>
2016-11-06 00:13:41 +0100

    test

--- src/pymove.py
+++ src/pymove.py
@@ -36,7 +36,7 @@ gpio.output(MOTOR_LEFT_EN2, True)
 gpio.output(MOTOR_RIGHT_EN1, True)
 gpio.output(MOTOR_RIGHT_EN2, True)
 
-GPIO.setup(HEAD_LEFT,GPIO.OUT)
+gpio.setup(HEAD_LEFT,GPIO.OUT)
 pwm = gpio.PWM(HEAD_LEFT, 50)
 pwm.start(5)
 

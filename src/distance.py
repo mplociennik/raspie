392dce35f7e937895040698ad1acaa3091c57388
Cieniu <marcin@laret.pl>
2016-11-05 21:30:46 +0100

    test

--- src/distance.py
+++ src/distance.py
@@ -26,13 +26,15 @@ class Distance:
             print "start"
             time.sleep(1)
             GPIO.output(TRIG,1)
-            time.sleep(1)
+            time.sleep(0.000001)
             GPIO.output(TRIG,0)
             while GPIO.input(ECHO) == 0:
-                print "pulse_start"
+                print "pulse start"
+                print GPIO.input(ECHO)
                 pulse_start = time.time()
             while GPIO.input(ECHO) == 1:
                 print "pulse_stop"
+                print GPIO.input(ECHO)
                 pulse_stop = time.time()
             distance = (pulse_stop - pulse_start) * 17150
             distance = round(distance, 2)

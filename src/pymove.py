fe63e406e43141684b329321567df03fc9587c15
Cieniu <marcin@laret.pl>
2016-11-05 18:53:25 +0100

    fix

--- src/pymove.py
+++ src/pymove.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from multiprocessing import Process, Queue
 import RPi.GPIO as gpio
 from distance import Distance
 
 
 MOTOR_RIGHT_EN1 = 7
-MOTOR_LEFT_EN2 = 11
+MOTOR_RIGHT_EN2 = 11
 MOTOR_LEFT_EN1 = 12
+MOTOR_LEFT_EN2 = 22
 MOTOR_RIGHT_DOWN = 13
 MOTOR_LEFT_DOWN = 16
 MOTOR_RIGHT_UP = 15

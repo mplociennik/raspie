783cde19dfcd874e392d91a57d34a52b12681e85
Cieniu <marcin@laret.pl>
2016-11-07 20:09:27 +0100

    test

--- src/servo.py
+++ src/servo.py
@@ -4,13 +4,14 @@ import RPi.GPIO as GPIO
 import time
 ''' http://makezine.com/projects/raspberry-eye-remote-servo-cam/ sprawdzic'''
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 pwm = GPIO.PWM(7, 50)
 pwm.start(7.5)
 
 try:
     while True:
+        print "cycle..."
         pwm.ChangeDutyCycle(7.5)
         time.sleep(1)
         pwm.ChangeDutyCycle(12.5)

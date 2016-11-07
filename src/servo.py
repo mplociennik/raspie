d1a6c7255d948cdeb48b05830daf9855561d7c2d
Cieniu <marcin@laret.pl>
2016-11-07 20:39:31 +0100

    test

--- src/servo.py
+++ src/servo.py
@@ -4,20 +4,28 @@ import RPi.GPIO as GPIO
 import time
 ''' http://makezine.com/projects/raspberry-eye-remote-servo-cam/ sprawdzic'''
 SERVO_Y = 33
+SERVO_X = 35
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(SERVO_Y, GPIO.OUT)
-pwm = GPIO.PWM(SERVO_Y, 50)
-pwm.start(7.5)
+pwm_Y = GPIO.PWM(SERVO_Y, 50)
+pwmY.start(7.5)
+GPIO.setup(SERVO_X, GPIO.OUT)
+pwm_X = GPIO.PWM(SERVO_Y, 50)
+pwm_X.start(7.5)
 
 try:
     while True:
         print "cycle..."
-        pwm.ChangeDutyCycle(7.5)
+        pwm_Y.ChangeDutyCycle(7.5)
+        pwm_X.ChangeDutyCycle(7.5)
         time.sleep(1)
-        pwm.ChangeDutyCycle(12.5)
+        pwm_Y.ChangeDutyCycle(12.5)
+        pwm_X.ChangeDutyCycle(12.5)
         time.sleep(1)
-        pwm.ChangeDutyCycle(2.5)
+        pwm._Y.ChangeDutyCycle(2.5)
+        pwm._X.ChangeDutyCycle(2.5)
         time.sleep(1)
 except KeyboardInterrupt:
-    pwm.stop()
+    pwm_Y.stop()
+    pwm_X.stop()
     GPIO.cleanup()

4c2b1b090f144534310ce3e9b453d778cc1db78a
Cieniu <marcin@laret.pl>
2016-11-17 21:11:16 +0100

    test

--- src/servo.py
+++ src/servo.py
@@ -17,10 +17,13 @@ try:
 #    while True:
     pwm_Y.start(7.5)
     pwm_X.start(7.5)
+    time.sleep(0.5)
     pwm_Y.stop()
     pwm_X.stop()
+    time.sleep(2)
     pwm_Y.start(2.5)
     pwm_X.start(2.5)
+    time.sleep(0.5)
     pwm_Y.stop()
     pwm_X.stop()
     time.sleep(2)

fae762013701253179ace0cf2267ece3e2814b51
Cieniu <marcin@laret.pl>
2016-11-17 21:02:53 +0100

    servo test

--- src/servo.py
+++ src/servo.py
@@ -8,26 +8,33 @@ SERVO_X = 13
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(SERVO_Y, GPIO.OUT)
 pwm_Y = GPIO.PWM(SERVO_Y, 50)
-pwm_Y.start(7.5)
+
 GPIO.setup(SERVO_X, GPIO.OUT)
 pwm_X = GPIO.PWM(SERVO_X, 50)
-pwm_X.start(7.5)
+
 
 try:
     while True:
-        print "cycle..."
-        pwm_Y.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-	pwm_X.ChangeDutyCycle(7.5)
+        pwm_Y.start(7.5)
+        pwm_X.start(7.5)
         time.sleep(1)
         pwm_Y.ChangeDutyCycle(12.5)
-        time.sleep(0.5)
-	pwm_X.ChangeDutyCycle(12.5)
-        time.sleep(1)
-        pwm_Y.ChangeDutyCycle(2.5)
-        time.sleep(0.5)
-	pwm_X.ChangeDutyCycle(2.5)
-        time.sleep(1)
+        pwm_X.ChangeDutyCycle(12.5)
+        pwm_Y.stop()
+        pwm_X.stop()
+#        print "cycle..."
+#        pwm_Y.ChangeDutyCycle(7.5)
+#        time.sleep(0.5)
+#	pwm_X.ChangeDutyCycle(7.5)
+#        time.sleep(1)
+#        pwm_Y.ChangeDutyCycle(12.5)
+#        time.sleep(0.5)
+#	pwm_X.ChangeDutyCycle(12.5)
+#        time.sleep(1)
+#        pwm_Y.ChangeDutyCycle(2.5)
+#        time.sleep(0.5)
+#	pwm_X.ChangeDutyCycle(2.5)
+#        time.sleep(1)
 except KeyboardInterrupt:
     pwm_Y.stop()
     pwm_X.stop()

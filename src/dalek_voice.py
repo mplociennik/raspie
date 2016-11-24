520d11902cf78033d51d66487562df7273fbd044
Cieniu <marcin@laret.pl>
2016-11-24 23:36:53 +0100

    update dalek

--- src/dalek_voice.py
+++ src/dalek_voice.py
@@ -1,4 +1,10 @@
 import os
 import sys
+import time
 
-os.system('')
+os.system('say -v Albert -o exterminate.aiff --data-format=BEI16@44100 \
+exterminate, exterminate, exterminate!')
+time.sleep(0.001)
+os.system('play exterminate.aiff stretch 1.2 133.33 lin 0.2 0.4 \
+overdrive 30 30 echo 0.4 0.8 15 0.8 \
+synth sine fmod 30 echo 0.8 0.8 29 0.8')

f53a06b625d83e9cda510f0fa087acd48dbe56a0
Cieniu <marcin@laret.pl>
2016-11-24 23:51:21 +0100

    test

--- src/dalek_voice.py
+++ src/dalek_voice.py
@@ -13,13 +13,13 @@ v = pyvona.create_voice(IVONA_ACCESS_KEY, IVONA_SECRET_KEY)
 v.voice_name = name
 v.region = region
 try:
-    v.fetch_voice(text, 'voice_file.aiff')
+    v.fetch_voice(text, 'voice_file')
 except:
     print "Speech: connection not found!"
     
-os.system('say -v Albert -o exterminate.aiff --data-format=BEI16@44100 \
-exterminate, exterminate, exterminate!')
+#os.system('say -v Albert -o exterminate.aiff --data-format=BEI16@44100 \
+exterminate, exterminate, exterminate!#')
 time.sleep(0.001)
-os.system('play exterminate.aiff stretch 1.2 133.33 lin 0.2 0.4 \
+os.system('play voice_file.ogg stretch 1.2 133.33 lin 0.2 0.4 \
 overdrive 30 30 echo 0.4 0.8 15 0.8 \
 synth sine fmod 30 echo 0.8 0.8 29 0.8')

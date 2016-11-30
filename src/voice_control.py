5885b8ea3f689e9f54ff449a952ff6459539d227
Marcin Płóciennik <mplociennik@trans.eu>
2016-11-30 14:30:51 +0100

    Update voice control

--- src/voice_control.py
+++ src/voice_control.py
@@ -8,7 +8,7 @@ from speech import Speech
 
 
 class VoiceControl():
-
+'''want to install pyaudio https://www.raspberrypi.org/forums/viewtopic.php?t=25173 '''
     def robot_autopilot(self):
         text = 'Starting autopilot'
         speech = Speech()
@@ -45,29 +45,26 @@ class VoiceControl():
 #            words = r.recognize_google(audio, None, 'pl-PL')
             words = r.recognize_sphinx(audio)
             print("PocketSphinx Speech Recognition thinks you said: " + words)
-            speech = Speech()
-            speech.create_voice(words)
-            if words == 'Raspie':
+            if 'Raspie' in words:
                 text = 'Listening you.'
                 speech = Speech()
                 speech.create_voice(text)
                 print text
-            if words == 'dupa':
-                text = 'Nie mów brzydko.'
-                speech = Speech()
-                speech.create_voice(text)
-                print text
-            if words == 'autopilot':
+            else if 'autopilot' in words:
                 text = 'Starting autopilot'
                 speech = Speech()
                 speech.create_voice(text)
                 self.robot_autopilot()
+            else if 'about' in words:
+                text = 'I\'m Raspie Robot version 1.0, home assistant created by Cieniu.'
+                speech = Speech()
+                speech.create_voice(text)
             self.listen_commands()
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
             self.listen_commands()
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            print("Could not request results from Google Speech Recognition service: {0}".format(e))
 
 if __name__ == '__main__':
     VoiceControl().listen_commands()

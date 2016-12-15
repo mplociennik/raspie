91691ee8625d9794a81df2d979edc8094e9bf124
Cieniu <marcin@laret.pl>
2016-12-15 22:06:20 +0100

    test

--- src/recorder.py
+++ src/recorder.py
@@ -13,13 +13,10 @@ class Recorder():
                        shell=True, preexec_fn=os.setsid) 
         time.sleep(5)
         os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
-        time.sleep(0.1)
-        cmd2 = 'aplay {0}/chunk.wav'.format(os.path.dirname(os.path.realpath(__file__)))
-        print cmd2
-        subprocess.call(cmd2)
-        return False
+        return 'Record has been saved!'
 
 
 if __name__ == "__main__":
     recorder = Recorder()
-    recorder.record()
+    test = recorder.record()
+    print test

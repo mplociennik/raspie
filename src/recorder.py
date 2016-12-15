ea99b262d7798bc9a2e774ebe993e5b6d3ade9e0
Cieniu <marcin@laret.pl>
2016-12-15 21:53:51 +0100

    fixit

--- src/recorder.py
+++ src/recorder.py
@@ -5,9 +5,7 @@ import signal
 import subprocess
 import time
 
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+
 class Recorder():
     def record(self):
         cmd = "arecord -D plughw:1,0 -f cd chunk.wav"
@@ -15,10 +13,10 @@ class Recorder():
                        shell=True, preexec_fn=os.setsid) 
         time.sleep(5)
         os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
-#        subprocess.call('arecord -D plughw:1,0 -f cd tmp/chunk.wav')
         time.sleep(0.1)
-#        subprocess.call('aplay chunk.wav')
+        subprocess.call('aplay chunk.wav')
         return False
+
 
 if __name__ == "__main__":
     recorder = Recorder()

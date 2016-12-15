c6711694cc7daaa74e10bf46c59ef4c17eef1804
Cieniu <marcin@laret.pl>
2016-12-15 21:40:09 +0100

    test

--- src/recorder.py
+++ src/recorder.py
@@ -8,8 +8,8 @@ import time
 # and open the template in the editor.
 class Recorder():
     def record(self):
-        process = subprocess.Popen("arecord -d 10 -D plughw:1,0 -f cd chunk.wav", shell=True)
-        time.sleep(2)
+        process = subprocess.Popen("arecord -d 5 -D plughw:1,0 -f cd chunk.wav", shell=True)
+        time.sleep(5.1)
         subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=process.pid))
 #        subprocess.call('arecord -D plughw:1,0 -f cd tmp/chunk.wav')
         subprocess.call('aplay chunk.wav')

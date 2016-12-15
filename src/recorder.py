a3480ab7882adab358647a684edbd33dae5b49cc
Cieniu <marcin@laret.pl>
2016-12-15 21:45:49 +0100

    test

--- src/recorder.py
+++ src/recorder.py
@@ -10,7 +10,7 @@ class Recorder():
     def record(self):
         process = subprocess.Popen("arecord -D plughw:1,0 -f cd chunk.wav", shell=True)
         time.sleep(5)
-        subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=process.pid))
+        process.terminate()
 #        subprocess.call('arecord -D plughw:1,0 -f cd tmp/chunk.wav')
         time.sleep(0.1)
         subprocess.call('aplay chunk.wav')

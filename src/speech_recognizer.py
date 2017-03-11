d8405eb682c1a06b57a0e1ba31f509587f280ac7
Cieniu <marcin@laret.pl>
2017-03-11 23:58:03 +0100

    test speech recognizer

--- src/speech_recognizer.py
+++ src/speech_recognizer.py
@@ -7,7 +7,7 @@ class SpeechRecognizer():
     def recognize(self):
         print 'listening...'
         p1 = subprocess.call('rec record.wav rate 16k silence 1 0.1 3% 1 3.0 3%',shell=True)
-        time.sleep(1)
+        time.sleep(0.1)
         print 'recognizing speech...'
         recognized_text = subprocess.check_output('pocketsphinx_continuous -infile record.wav -lm models/5835.lm -dict models/5835.dic -logfn /dev/null',shell=True, stderr=subprocess.STDOUT)
         os.remove('record.wav')

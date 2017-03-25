708c278de13ad6ce1564d0db6a4a4ea5db4edeec
Cieniu <marcin@laret.pl>
2017-03-25 11:13:44 +0100

    fix test voice control

--- src/raspie.py
+++ src/raspie.py
@@ -16,7 +16,7 @@ def key_control():
     return key_control
 
 def voice_commands():
-     voice_control_process = VoiceControl().start()
+    voice_control_process = VoiceControl().start()
 
 def cam_recording():
     print "cam_recording"
@@ -29,18 +29,14 @@ if __name__ == '__main__':
     jobs = []
 #    webapi = multiprocessing.Process(target=webapi)
     key_control = multiprocessing.Process(target=key_control)
-    voice_control = VoiceControl()
-    voice_control.start()
+    voice_control()
 #    cam_recording = multiprocessing.Process(target=cam_recording)
     welcome = multiprocessing.Process(target=welcome)
 #    jobs.append(webapi)
     jobs.append(key_control)
-    jobs.append(voice_control)
     jobs.append(cam_recording)
     jobs.append(welcome)
-    
 #    webapi.start()
     key_control.start()
-    voice_control.start()
 #    cam_recording.start()
     welcome.start()
